4e5078b88a5458ff26676e93a9ed9a33c0b7b33e
mrotante <michael.rotante@uconn.edu>
2024-01-22 00:31:57 -0500

    css cleanup

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,52 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import dividingBar from "./images/dividing-bar-home-1@2x.png";
+import helpButton from "./images/help-1@2x.png";
+import playButton from "./images/play-1@2x.png";
+import statsButton from "./images/stats-1@2x.png";
+import pauseButton from "./images/pause.png"
+
 import "./index.css";
 
 export const App = () => {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="index">
       <div className="div">
-        <p className="SAMP-LE">
+        <div className="wrapper">
           <span className="text-wrapper">SAMP.LE</span>
-        </p>
-        <img
-          className="dividing-bar-home"
-          alt="Dividing bar home"
-          src="https://cdn.animaapp.com/projects/65a55ad4c2389bf8d304f5bc/releases/65a56103b4a03616a9330fc6/img/dividing-bar-home-1@2x.png"
-        />
+        </div>
         <img
           className="help"
           alt="Help"
-          src="https://cdn.animaapp.com/projects/65a55ad4c2389bf8d304f5bc/releases/65a56103b4a03616a9330fc6/img/help-1@2x.png"
+          src={helpButton}
         />
         <img
           className="stats"
           alt="Stats"
-          src="https://cdn.animaapp.com/projects/65a55ad4c2389bf8d304f5bc/releases/65a56103b4a03616a9330fc6/img/stats-1@2x.png"
+          src={statsButton}
         />
-        <div className="entry" />
-        <div className="entry-2" />
-        <div className="entry-3" />
-        <div className="entry-4" />
-        <div className="entry-5" />
-        <div className="entry-6" />
-        <div className="media-player-bar" />
-        <div className="overlap">
-          <img
-            className="play"
-            alt="Play"
-            src="https://cdn.animaapp.com/projects/65a55ad4c2389bf8d304f5bc/releases/65a56103b4a03616a9330fc6/img/play-1@2x.png"
-          />
-        </div>
-        <div className="user-entry" />
-        <div className="overlap-group">
-          <p className="SKIP">
-            <span className="span">SKIP</span>
-          </p>
-        </div>
-        <div className="SUBMIT-wrapper">
-          <p className="SUBMIT">
-            <span className="span">SUBMIT</span>
-          </p>
+        <img
+          className="dividing-bar-home"
+          alt="Dividing bar home"
+          src={dividingBar}
+        />
+        <div className="entry-container">
+          <div className="entry" />
+          <div className="entry" />
+          <div className="entry" />
+          <div className="entry" />
+          <div className="entry" />
+          <div className="entry" />
+          <div className="media-player-bar" />
+          <div className="overlap">
+            {isPlaying ? <img
+                className="play"
+                alt="Play"
+                src={playButton}
+                onClick={() => {
+                  setIsPlaying(false)
+                }}
+              /> : <img
+              className="pause"
+              alt="pause"
+              src={pauseButton}
+              onClick={() => {
+                setIsPlaying(true)
+              }}
+            />}
+          </div>
+          <div className="user-entry" />
+          <div className="buttons">
+            <button className="input-button">SKIP</button>
+            <button className="input-button">SUBMIT</button>
+          </div>
         </div>
       </div>
     </div>
